6cc9c04b917ae8e9ab9fc58be135278ae7f87eea
BANI GIT <bani.rezaie@gmail.com>
2021-07-27 17:52:39 +0100

    redux + middleware + redux dev tools applied

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CoursesPage() {
-  return <h2>Courses</h2>;
+  const [course, setCourse] = useState({
+    title: "",
+  });
+
+  function handleChange(e) {
+    const _course = { ...course, title: e.target.value };
+    setCourse(_course);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert(course.title);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Courses</h2>
+      <h3>Add course</h3>
+      <input type='text' onChange={handleChange} value={course.title} />
+      <input type='submit' value='Save' />
+    </form>
+  );
 }
 
 export default CoursesPage;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./components/App";
 import "./index.css";
+import configureStore from "./redux/configureStore";
+import { Provider } from "react-redux";
+
+const store = configureStore();
 
 render(
-  <BrowserRouter>
-    <App className='nav' id='app' />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App className='nav' id='app' />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("app")
 );
